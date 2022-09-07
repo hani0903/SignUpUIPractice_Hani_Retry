2a0f5bfa52fb6991f77abe65debc375fd088392d
Hani <jhe93@naver.com>
2022-09-07 23:07:06 +0900

    Initial commit

---
+++ app/src/main/java/com/metacoding/signupuipractice_hani_retry/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.metacoding.signupuipractice_hani_retry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
